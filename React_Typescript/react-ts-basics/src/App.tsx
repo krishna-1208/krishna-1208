32cbc6e7c9242aaedd80e11d7fc792c4e9bdcabd
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-12-10 12:49:51 +0530

    Adding child props in ts file

--- React_Typescript/react-ts-basics/src/App.tsx
+++ React_Typescript/react-ts-basics/src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
       title="Learn React + TS" 
       description="Learn how to build a React app with TS.">
         <h5>This is additional children prop.</h5>
+        <p>Adding more child props in code</p>
       </CourseGoal>
   </main>
 }
